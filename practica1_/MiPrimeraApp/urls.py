2280c0a5d8d3f2dbeb434990ec0de48a21a9a90d
Clauane <97339933+Clauane@users.noreply.github.com>
2025-02-14 23:02:45 +0100

    Add multiple views and URLs

--- practica1_/MiPrimeraApp/urls.py
+++ practica1_/MiPrimeraApp/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('bienvenida/',views.bienvenida, name = 'bienvenida'),
 ]
 
+urlpatterns = [
+    path('bienvenida1/',views.bienvenida1, name = 'bienvenida1'),
+]
+
+urlpatterns = [
+    path('bienvenida2/',views.bienvenida2, name = 'bienvenida2'),
+]
--- practica1_/MiPrimeraApp/views.py
+++ practica1_/MiPrimeraApp/views.py
@@ -4,6 +4,17 @@ from django.http import HttpResponse
 def bienvenida(request):
     return HttpResponse("hola esta es la pagina de inicio de tu aplicación ")
 
+def bienvenida1(request):
+    return HttpResponse("hola esta es la pagina de algo no se de tu aplicación ")
+
+
+def bienvenida2(request):
+    return HttpResponse("hola esta es la pagina del final de tu aplicación ")
+
+
+
+
+
 
 
 # Create your views here.
